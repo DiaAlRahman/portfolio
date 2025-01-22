068e5bac65524a7a040a9a51650c0504034f96d8
DiaAlRahman <127028600+DiaAlRahman@users.noreply.github.com>
2025-01-22 09:16:21 +0600

    add animated navbar, finally!

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -4,6 +4,7 @@ import { navLinks } from '../constants';
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => setIsOpen((prev) => !prev);
+  const shutMenu = () => setIsOpen(false);
   const NavItems = () => {
     return (
       <ul className='nav-ul'>
@@ -18,15 +19,20 @@ const Navbar = () => {
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
       <div className="max-w-7xl mx-auto">
-        <div className="flex justify-between items-center py-5 mx-auto c-space">
+        <div onMouseLeave={shutMenu} className="flex justify-between items-center py-5 mx-auto c-space">
           <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">Dia</a>
-          <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none flex sm:hidden' aria-label='Toggle Menu'>
-            <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt='menu' className='w-6 h-6'/>
-          </button>
-          <nav className="sm:flex hidden">
+          <nav className={`falling-navbar ${isOpen ? 'translate-y-0' : '-translate-y-[50vh]'}`}>
             <NavItems />
           </nav>
+          <button onMouseOver={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none flex' aria-label='Toggle Menu'>
+            <img src={"assets/menu.svg"} alt='menu' className='w-6 h-6'/>
+          </button>
         </div>
+      </div>
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className="p-5">
+          <NavItems />
+        </nav>
       </div>
     </header>
   )
