f78e308d167731704f74eaf625d7437af80e4718
DiaAlRahman <127028600+DiaAlRahman@users.noreply.github.com>
2025-01-23 12:03:29 +0600

    add files for uploading

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,14 @@
-import { useEffect, useState } from "react"
-import { heroTags } from "../constants";
+import { Suspense, useEffect, useState } from "react";
+import { calculateSizes, heroTags } from "../constants";
+import { Canvas } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import HackerRoom from "../components/HackerRoom";
+import CanvasLoader from "../components/CanvasLoader";
+// import { Leva, useControls } from "leva";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-  const HeroTags = ({ contents, interval = 3000 }) => {
+  const HeroTags = ({ contents, interval = 10000 }) => {
     const [i, setCurr] = useState(0)
 
     useEffect(() => {
@@ -22,11 +28,69 @@ const Hero = () => {
         </div>
     );
   };
+  // const controls = useControls('HackerRoom', {
+  //   positionX: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionY: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   positionZ: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationX: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationY: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   rotationZ: {
+  //     value: 0,
+  //     min: -10,
+  //     max: 10,
+  //   },
+  //   scale: {
+  //     value: 1,
+  //     min: 0.1,
+  //     max: 2,
+  //   },
+  //   })
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col items-center sm:mt-36 mt-20 c-space gap-3">
         <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsans">Hi, I am Dia <span className="waving-hand">😉</span></p>
-        <HeroTags contents={heroTags} interval={5000}/>
+        <HeroTags contents={heroTags} />
+        <div className="w-full h-full absolute inset-0">
+          <Canvas className="w-full h-full">
+            <Suspense fallback={<CanvasLoader />}>
+              <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+              <HackerRoom
+                // scale={0.07}
+                position={sizes.deskPosition}
+                rotation={[0, -Math.PI - 0.5, 0]}
+                scale={sizes.deskScale}
+              />
+              <ambientLight intensity={1} />
+              <directionalLight intensity={0.5} position={[10, 10, 10]} />
+            </Suspense>
+          </Canvas>
+        </div>
       </div>
     </section>
   )
