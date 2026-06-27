1c829f204e813d94d72fc3252d3455b92da7cc84
Dia <a1882857@adelaide.edu.au>
2026-06-28 08:38:01 +1000

    add linkedin account

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,15 +4,20 @@ const Footer = () => {
       <div className="text-white-500 flex gap-2">
         <div className="flex gap-3">
           <div className="social-icon">
-            <a href="https://github.com/DiaAlRahman" target="_blank" rel="noreferrer" className="flex justify-center w-full h-full">
-              <img src="/portfolio/assets/github.svg" alt="github" className="w-1/2 h-1/2 cursor-pointer" />
+            <a href="https://github.com/DiaAlRahman" target="_blank" rel="noreferrer" className="flex justify-center">
+              <img src="/portfolio/assets/github.svg" alt="github" className="w-6 h-6 cursor-pointer" />
             </a>
           </div>
           <div className="social-icon">
-            <a href="https://x.com/namless_child" target="_blank" rel="noreferrer" className="flex justify-center w-full h-full">
-              <img src="/portfolio/assets/twitter.svg" alt="twitter" className="w-1/2 h-1/2 cursor-pointer" />
+            <a href="https://www.linkedin.com/in/dia-al-rahman-898994248/" target="_blank" rel="noreferrer" className="flex justify-center">
+              <img src="/portfolio/assets/linkedin.png" alt="linkedin" className="w-6 h-6 cursor-pointer" />
             </a>
           </div>
+          {/* <div className="social-icon">
+            <a href="https://x.com/namless_child" target="_blank" rel="noreferrer" className="flex justify-center">
+              <img src="/portfolio/assets/twitter.svg" alt="twitter" className="w-6 h-6 cursor-pointer" />
+            </a>
+          </div> */}
           {/* <div className="social-icon">
             <a href="https://www.instagram.com/nameless._.child" target="_blank" rel="noreferrer" className="flex justify-center">
               <img src="portfolio/assets/instagram.svg" alt="instagram" className="w-1/2 h-1/2 cursor-pointer" />
